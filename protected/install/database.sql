6492cef95a5fcb4a1e7371c15270abb3da76c0d4
John Hobbs <john@velvetcache.org>
2010-01-15 15:56:29 -0600

    Added the storytlr-plugin-github into the mix.

--- protected/install/database.sql
+++ protected/install/database.sql
@@ -962,3 +962,20 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2009-12-03 22:20:43
+
+DROP TABLE IF EXISTS `github_data`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `github_data` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `source_id` varchar(255) NOT NULL,
+  `title` text NOT NULL,
+  `content` text,
+  `repository` text,
+  `link` varchar(255) NOT NULL,
+  `published` varchar(45) NOT NULL,
+  PRIMARY KEY  USING BTREE (`id`),
+  UNIQUE KEY `DUPLICATES` USING BTREE (`source_id`),
+  FULLTEXT KEY `SEARCH` (`title`,`repository`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
