34e40b2fc03967f048dcfb49fcf99e64cf8bccc1
John Hobbs <john@velvetcache.org>
2010-01-15 15:32:35 -0600

    Baked in the latest Zend. Not everyone runs it on their server.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-define("STORYTLR_VERSION","0.9.3.dev");
+define("STORYTLR_VERSION","0.9.3.jmhobbs.dev");
 
 // Update after deployment for location of non-public files
 $root = dirname(__FILE__);
@@ -7,7 +7,8 @@ $root = dirname(__FILE__);
 // We're assuming the Zend Framework is already on the include_path
 // TODO this should be moved to the boostrap file
 set_include_path(
-      $root . '/protected/application' . PATH_SEPARATOR
+		  $root . '/protected/library/Zend' . PATH_SEPARATOR
+    . $root . '/protected/application' . PATH_SEPARATOR
     . $root . '/protected/application/admin/models' . PATH_SEPARATOR
     . $root . '/protected/application/public/models' . PATH_SEPARATOR
     . $root . '/protected/application/pages/models' . PATH_SEPARATOR
