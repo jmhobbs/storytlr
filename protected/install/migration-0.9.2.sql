4b1d8d662c69f8c353207c736b93599e2a92740f
John Hobbs <john@velvetcache.org>
2010-02-02 15:21:02 -0600

    Added the Stack Overflow plugin from http://github.com/jmhobbs/storytlr-plugin-stackoverflow

--- protected/install/migration-0.9.2.sql
+++ protected/install/migration-0.9.2.sql
@@ -50,3 +50,20 @@ CREATE TABLE `goodreads_data` (
 SET character_set_client = @saved_cs_client;
 
 ALTER TABLE `data` ADD COLUMN `is_unwanted` TINYINT(1)  NOT NULL DEFAULT 0 AFTER `timestamp`;
+
+DROP TABLE IF EXISTS `stackoverflow_data`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `stackoverflow_data` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `source_id` int(10) unsigned NOT NULL,
+  `stackoverflow_id` varchar(255) NOT NULL,
+  `title` text NOT NULL,
+  `content` text,
+  `link` varchar(255) NOT NULL,
+  `published` varchar(45) NOT NULL,
+  PRIMARY KEY  USING BTREE (`id`),
+  UNIQUE KEY `DUPLICATES` USING BTREE (`source_id`, `stackoverflow_id`),
+  FULLTEXT KEY `SEARCH` (`content`,`title`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
