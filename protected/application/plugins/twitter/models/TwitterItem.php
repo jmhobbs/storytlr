25b738ffc6a2f38a3005ebfa2ca820cce9c53b68
John Hobbs <john@velvetcache.org>
2010-02-02 11:23:10 -0600

    Added support for pictures from Brizzly to the Twitter plugin.

--- protected/application/plugins/twitter/models/TwitterItem.php
+++ protected/application/plugins/twitter/models/TwitterItem.php
@@ -1,6 +1,7 @@
 <?php
 /*
- *    Copyright 2008-2009 Laurent Eschenauer and Alard Weisscher
+ *  Copyright 2008-2009 Laurent Eschenauer and Alard Weisscher
+ *  Copyright 2010 John Hobbs
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -13,64 +14,64 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *  
+ *
  */
 class TwitterItem extends SourceItem {
 
 	protected $_prefix 	= 'twitter';
-	
+
 	public function toArray() {
 		$this->_data['tweet'] = $this->getStatus();
 		return $this->_data;
 	}
-	
+
 	public function getType() {
 		if ($this->_data['photo_service']) {
-			return SourceItem::IMAGE_TYPE;			
+			return SourceItem::IMAGE_TYPE;
 		} else {
 			return SourceItem::STATUS_TYPE;
 		}
 	}
-	
+
 	public function getStatus() {
 		$status = htmlspecialchars($this->_data['text']);
-		
-		if ($this->_data['photo_service'] == 'twitpic') {	
+
+		if ($this->_data['photo_service'] == 'twitpic') {
 			$status = preg_replace("/http:\/\/twitpic\.com\/\w+/", "", $status);
-		} elseif ($this->_data['photo_service'] == 'phodroid') {	
+		} elseif ($this->_data['photo_service'] == 'phodroid') {
 			$status = preg_replace("/http:\/\/phodroid\.com\/\w+/", "", $status);
 		}
-		
-		return $status; 
+
+		return $status;
 	}
-	
+
 	public function setStatus($status) {
 		$db = Zend_Registry::get('database');
-		
+
 		$sql = "UPDATE `twitter_data` SET `text`=:status "
 			 . "WHERE source_id = :source_id AND id = :item_id ";
-		
+
 		$data 		= array("source_id" 	=> $this->getSource(),
 							"item_id"		=> $this->getID(),
 							"status"		=> $status);
-							
+
  		$stmt 	= $db->query($sql, $data);
 
  		return;
 	}
-	
+
 	public function getTitle() {
 		return $this->getStatus();
 	}
-	
+
 	public function setTitle($title) {
 		$this->setStatus($title);
 	}
-	
+
 	public function getDescription() {
 		return $this->_data['note'];
 	}
-	
+
 	public function setDescription($note) {
 		$db = Zend_Registry::get('database');
 		$sql = "UPDATE `twitter_data` SET `note`=:note "
@@ -78,12 +79,12 @@ class TwitterItem extends SourceItem {
 		$data 		= array("source_id" 	=> $this->getSource(),
 							"item_id"		=> $this->getID(),
 							"note"			=> $note);
-							
+
  		return $db->query($sql, $data);
 	}
-	
+
 	public function getImageUrl($size=ImageItem::SIZE_MEDIUM) {
-		if ($this->_data['photo_service'] == 'twitpic') {	
+		if ($this->_data['photo_service'] == 'twitpic') {
 			switch ($size) {
 				case ImageItem::SIZE_THUMBNAIL:
 					return "http://twitpic.com/show/thumb/{$this->_data['photo_key']}.jpg";
@@ -96,36 +97,54 @@ class TwitterItem extends SourceItem {
 					break;
 				case ImageItem::SIZE_LARGE:
 					return "http://twitpic.com/show/large/{$this->_data['photo_key']}.jpg";
-					break;												
+					break;
 				case ImageItem::SIZE_ORIGINAL:
 					return "http://twitpic.com/show/large/{$this->_data['photo_key']}.jpg";
 					break;
 			}
 		}
-		elseif  ($this->_data['photo_service'] == 'phodroid') {	
+		elseif  ($this->_data['photo_service'] == 'phodroid') {
 			return "http://s.phodroid.com/{$this->_data['photo_key']}.jpg";
+		}
+		elseif ( $this->_data['photo_service'] == 'brizzly' ) {
+			switch ($size) {
+				default:
+				case ImageItem::SIZE_THUMBNAIL:
+				case ImageItem::SIZE_SMALL:
+					return "http://pics.brizzly.com/thumb_sm_{$this->_data['photo_key']}.jpg";
+					break;
+				case ImageItem::SIZE_MEDIUM:
+				case ImageItem::SIZE_LARGE:
+					return "http://pics.brizzly.com/thumb_lg_{$this->_data['photo_key']}.jpg";
+					break;
+				case ImageItem::SIZE_ORIGINAL:
+					return "http://pics.brizzly.com/{$this->_data['photo_key']}.jpg";
+					break;
+			}
 		}
 		else {
 			return false;
 		}
 	}
-	
+
 	public function getLink() {
 		if ($this->getType() == SourceItem::IMAGE_TYPE) {
-			if ($this->_data['photo_service'] == 'twitpic') {	
+			if ($this->_data['photo_service'] == 'twitpic') {
 				return "http://twitpic.com/{$this->_data['photo_key']}";
-			} elseif  ($this->_data['photo_service'] == 'phodroid') {	
+			} elseif  ($this->_data['photo_service'] == 'phodroid') {
 				return "http://phodroid.com/{$this->_data['photo_key']}";
-			} 
+			} elseif  ($this->_data['photo_service'] == 'brizzly') {
+				return "http://brizzly.com/pic/{$this->_data['photo_key']}";
+			}
 		}
-		
+
 		$properties = new SourcesProperties(array(Stuffpress_Db_Properties::KEY => $this->_data['source_id']));
 		$username   = $properties->getProperty('username');
 		$url 		= "http://twitter.com/$username/statuses/{$this->_data['twitter_id']}";
-		
+
 		return $url;
 	}
-	
+
 	public function getRssContent() {
 		$url  = $this->getPhotoURL('-');
 		$link = $this->getLink();
@@ -133,7 +152,7 @@ class TwitterItem extends SourceItem {
 		$html = "<a href='$link'><img src='$url' alt='$title'></a>";
 		return $html;
 	}
-	
+
 	public function getBackup() {
 		$item = array();
 		$item['Id']					= $this->_data['twitter_id'];
--- protected/application/plugins/twitter/models/TwitterModel.php
+++ protected/application/plugins/twitter/models/TwitterModel.php
@@ -1,6 +1,7 @@
 <?php
 /*
- *    Copyright 2008-2009 Laurent Eschenauer and Alard Weisscher
+ *  Copyright 2008-2009 Laurent Eschenauer and Alard Weisscher
+ *  Copyright 2010 John Hobbs
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -13,7 +14,7 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *  
+ *
  */
 class TwitterModel extends SourceModel {
 
@@ -22,8 +23,8 @@ class TwitterModel extends SourceModel {
 	protected $_prefix  = 'twitter';
 
 	protected $_search  = 'text';
-	
-	protected $_update_tweet = "Updated %d times my Twitter status %s"; 
+
+	protected $_update_tweet = "Updated %d times my Twitter status %s";
 
 	public function getServiceName() {
 		return "Twitter";
@@ -45,14 +46,14 @@ class TwitterModel extends SourceModel {
 	public function isStoryElement() {
 		return true;
 	}
-	
+
 	public function importData() {
 		$username = $this->getProperty('username');
 
 		if (!$username) {
 			throw new Stuffpress_Exception("Update failed, connector not properly configured");
 		}
-		
+
 		// Proceed with the update
 		$items = $this->updateData(true);
 
@@ -67,7 +68,7 @@ class TwitterModel extends SourceModel {
 		$pwd   = $config->twitter->password;
 		$pages = $import ? 32  : 1;
 		$count = $import ? 100 : 50;
-		
+
 		// Get user properties
 		$username = $this->getProperty('username');
 		$uid	  = $this->getProperty('uid', 0);
@@ -75,10 +76,10 @@ class TwitterModel extends SourceModel {
 		if (!$username) {
 			throw new Stuffpress_Exception("Update failed, connector not properly configured");
 		}
-		
+
 		// Update the user uid if required
 		if (!$uid || $uid==0) {
-			$uid = $this->getTwitterUid($username);	
+			$uid = $this->getTwitterUid($username);
 			if ($uid > 0) {
 				$this->setProperty('uid', $uid);
 			}
@@ -89,15 +90,15 @@ class TwitterModel extends SourceModel {
 		$curl = curl_init();
 		curl_setopt($curl, CURLOPT_HEADER, false);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_USERPWD, "$user:$pwd"); 
+		curl_setopt($curl, CURLOPT_USERPWD, "$user:$pwd");
 		curl_setopt($curl, CURLOPT_USERAGENT,'Storytlr/1.0');
-		
+
 		for($page=1; $page<= $pages; $page ++) {
 			$url  = "http://twitter.com/statuses/user_timeline/$username.xml?count=$count&page=$page";
 			curl_setopt($curl, CURLOPT_URL, $url);
 			$response = curl_exec($curl);
 			$http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-			
+
 			if ($http_code != 200) {
 				throw new Stuffpress_Exception("Twitter API returned http status $http_code for url: $url", $http_code);
 			}
@@ -110,20 +111,20 @@ class TwitterModel extends SourceModel {
 				break;
 			}
 
-			$items = $this->processItems($data);	
+			$items = $this->processItems($data);
 			$result = array_merge($result,$items);
-			
+
 			if (count($data) < $count) {
 				break;
 			}
 		}
-		
-		curl_close ($curl);			
+
+		curl_close ($curl);
 		unset($curl);
 
 		// Mark as updated (could have been with errors)
 		$this->markUpdated();
-		
+
 		return $result;
 	}
 
@@ -135,7 +136,7 @@ class TwitterModel extends SourceModel {
 		$element = $form->createElement('text', 'username', array('label' => $label , 'decorators' => $form->elementDecorators));
 		$element->setRequired(true);
 		$form->addElement($element);
-		
+
 		// Options
 		$options = array();
 		if ($this->getPropertyDefault('hide_replies')) $options[] = 'hide_replies';
@@ -144,11 +145,11 @@ class TwitterModel extends SourceModel {
 			'multiOptions' => array(
 			'hide_replies' 	=> 'Hide @replies tweets'
 			)
-		)); 
+		));
 		$e->setLabel('Options');
 		$e->setValue($options);
 		$form->addElement($e);
-		
+
 		if($populate) {
 			$options = array();
 			$values  = $this->getProperties();
@@ -171,7 +172,7 @@ class TwitterModel extends SourceModel {
 			$this->_properties->setProperty('username',   $username);
 			$update = true;
 		}
-		
+
 		// Save hide_replies property
 		$hide_replies = @in_array('hide_replies',$options) ? 1 : 0;
 		$this->_properties->setProperty('hide_replies', $hide_replies);
@@ -180,24 +181,24 @@ class TwitterModel extends SourceModel {
 	}
 
 	public function processGnipItem($activity) {
-		
+
 		if (!$item = @new SimpleXMLElement($activity->payload->decodedRaw())) {
 			return;
 		}
-		
+
 		if ($activity->action != 'notice') {
 			return;
 		}
-		
+
 		$text							= $item->title;
 		$text							= mb_substr($text, strpos($text, ':') + 2);
-		
+
 		$tags = $this->getTags($text);
-		
+
 		$data = array();
 		if ($photo = $this->getPhoto($text)) {
 				$data['photo_key'] 			= $photo['key'];
-				$data['photo_service']		= $photo['service'];					
+				$data['photo_service']		= $photo['service'];
 		}
 		$data['created_at'] 			= $item->published;
 		$data['twitter_id']				= $item->status_id;
@@ -209,26 +210,26 @@ class TwitterModel extends SourceModel {
 
 		$hide_replies = $this->getProperty('hide_replies');
 		$is_reply  = (mb_substr($text, 0, 1) == '@') ? true : false;
-		$is_repost = @in_array(strip_tags($data['source']), array('storytlr'));		
+		$is_repost = @in_array(strip_tags($data['source']), array('storytlr'));
 		$is_hidden = ($is_repost || ($is_reply && $hide_replies)) ? 1 : 0;
- 		$type = $photo ? SourceItem::IMAGE_TYPE : SourceItem::STATUS_TYPE;       		
-		
+ 		$type = $photo ? SourceItem::IMAGE_TYPE : SourceItem::STATUS_TYPE;
+
  		if ($is_repost) return;
- 		
+
  		$this->addItem($data, strtotime($item->published), $type, $tags, false, $is_hidden, $data['text']);
 	}
-	
-	
+
+
 	private function processItems($items) {
 		$result = array();
 		if ($items && count($items)>0) foreach ($items as $item) {
 			$data = array();
-			
+
 			$tags = $this->getTags((string) $item->text);
-			
+
 			if ($photo = $this->getPhoto((string) $item->text)) {
 				$data['photo_key'] 			= $photo['key'];
-				$data['photo_service']		= $photo['service'];					
+				$data['photo_service']		= $photo['service'];
 			}
 			$data['created_at'] 			= (string) $item->created_at;
 			$data['twitter_id']				= (string) $item->id;
@@ -243,26 +244,26 @@ class TwitterModel extends SourceModel {
 			$is_repost = @in_array(strip_tags($data['source']), array('storytlr'));
 			$is_hidden = ($is_repost || ($is_reply && $hide_replies)) ? 1 : 0;
 			$type = $photo ? SourceItem::IMAGE_TYPE : SourceItem::STATUS_TYPE;
-			
+
 			if ($is_repost) continue;
 
 			$id = $this->addItem($data, strtotime($data['created_at']), $type, $tags, false, $is_hidden, $data['text']);
-			
-			if ($id) $result[] = $id;	
+
+			if ($id) $result[] = $id;
 			unset($data);
 		}
 		return $result;
 	}
-	
+
 	public function processItem($item) {
 		$result = array();
 		$tags = $this->getTags((string) $item->text);
-		
+
 		if ($photo = $this->getPhoto((string) $item->text)) {
 			$data['photo_key'] 			= $photo['key'];
-			$data['photo_service']		= $photo['service'];					
+			$data['photo_service']		= $photo['service'];
 		}
-		
+
 		$data['created_at'] 			= (string) $item->created_at;
 		$data['twitter_id']				= (string) $item->id;
 		$data['text'] 					= (string) $item->text;
@@ -276,34 +277,40 @@ class TwitterModel extends SourceModel {
 		$is_repost = @in_array(strip_tags($data['source']), array('storytlr'));
 		$is_hidden = ($is_repost || ($is_reply && $hide_replies)) ? 1 : 0;
 		$type = $photo ? SourceItem::IMAGE_TYPE : SourceItem::STATUS_TYPE;
-		
+
 		if ($is_repost) continue;
 
 		$id = $this->addItem($data, strtotime($data['created_at']), $type, $tags, false, $is_hidden, $data['text']);
-		if ($id) $result[] = $id;	
-			
+		if ($id) $result[] = $id;
+
 		return $result;
 	}
-	
+
 	private function getPhoto($status) {
 		$matches = array();
-		
+
 		// Do we have a twitpic ?
 		if (preg_match("/twitpic.com\/(\w+)/i",$status,$matches)) {
 			$photo['key'] = $matches[1];
 			$photo['service'] = 'twitpic';
 			return $photo;
-		} 
+		}
 		// Do we have an android ?
 		elseif (preg_match("/phodroid.com\/(\w+)/i",$status,$matches)) {
 			$photo['key'] = $matches[1];
 			$photo['service'] = 'phodroid';
 			return $photo;
+		}
+		// Do we have brizzly?s
+		elseif (preg_match("/brizzly.com\/pic\/(\w+)/i",$status,$matches)) {
+			$photo['key'] = $matches[1];
+			$photo['service'] = 'brizzly';
+			return $photo;
 		} else {
 			return false;
 		}
 	}
-	
+
 	private function getTags($status) {
 		$matches 	= array();
 		preg_match_all("/#(\w+)/",$status,$matches);
@@ -317,33 +324,33 @@ class TwitterModel extends SourceModel {
 		echo "Fetched the ID $id for Twitter.\r\n";
 		return $id;
 	}
-	
+
 	private function getTwitterUid($username) {
 		$config = Zend_Registry::get("configuration");
 		$user  = $config->twitter->username;
-		$pwd   = $config->twitter->password;	
+		$pwd   = $config->twitter->password;
 
 		$curl = curl_init();
 		curl_setopt($curl, CURLOPT_HEADER, false);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_USERPWD, "$user:$pwd"); 
+		curl_setopt($curl, CURLOPT_USERPWD, "$user:$pwd");
 		curl_setopt($curl, CURLOPT_USERAGENT,'Storytlr/1.0');
-		
+
 		$url  = "http://twitter.com/users/show.json?screen_name=$username";
 		curl_setopt($curl, CURLOPT_URL, $url);
 		$response = curl_exec($curl);
 		$http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-				
+
 		if ($http_code != 200) {
 			return 0;
-		}		
+		}
 
 		if ($response && strlen($response) > 0) {
 			$twitter_user = json_decode($response);
 			$uid = $twitter_user->id;
 			return $uid;
-		}	
-		
+		}
+
 		return 0;
 	}
 }
