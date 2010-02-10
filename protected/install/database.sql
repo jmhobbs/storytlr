13458e494ef60be417f85277355938872526e18f
John Hobbs <john@velvetcache.org>
2010-02-10 11:20:34 -0600

    Added Google Buzz plugin from http://github.com/jmhobbs/storytlr-plugin-googlebuzz.

--- protected/install/database.sql
+++ protected/install/database.sql
@@ -1032,3 +1032,20 @@ CREATE TABLE `stackoverflow_data` (
   FULLTEXT KEY `SEARCH` (`content`,`title`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 SET character_set_client = @saved_cs_client;
+
+DROP TABLE IF EXISTS `googlebuzz_data`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `googlebuzz_data` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `source_id` int(10) unsigned NOT NULL,
+  `buzz_id` varchar(255) NOT NULL,
+  `title` text NOT NULL,
+  `content` text,
+  `link` varchar(255) NOT NULL,
+  `published` varchar(45) NOT NULL,
+  PRIMARY KEY  USING BTREE (`id`),
+  UNIQUE KEY `DUPLICATES` USING BTREE (`source_id`, `buzz_id`),
+  FULLTEXT KEY `SEARCH` (`content`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
