f33c2e40e603c8820aea09de0b2ef97b2820bea5
John Hobbs <john@velvetcache.org>
2010-02-18 12:04:43 -0600

    Added the blip.tv plugin from http://github.com/jmhobbs/storytlr-plugin-bliptv

---
+++ protected/application/plugins/bliptv/README.markdown
@@ -0,0 +1,18 @@
+# What is this? #
+
+This is a plugin for the Storytlr application.  It allows you to integrate your blip.tv videos into the Storytlr lifestream.
+
+# Installation #
+
+**WARNING**
+
+Unless you are running an up to date copy of [my fork](http://github.com/jmhobbs/storytlr) you need to apply a bugfix! <http://github.com/jmhobbs/storytlr/commit/4b58d8c9283f3bd85accae799dc23fd64a093d86>
+
+**Okay, fixed that? Continue.**
+
+1. Clone this repo into your storytlr/protected/application/plugins/ directory
+2. Rename the directory from "storytlr-plugin-bliptv" to "bliptv"
+3. Load database.sql into your database
+4. Copy bliptv.png into storytlr/images/
+5. Go on the admin side and add your account
+6. You were done at number 5, you can stop reading now
--- protected/install/database_0.9.2.sql
+++ protected/install/database_0.9.2.sql
@@ -1067,3 +1067,25 @@ CREATE TABLE `twitterfavorites_data` (
   FULLTEXT KEY `SEARCH` (`content`,`title`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 SET character_set_client = @saved_cs_client;
+
+DROP TABLE IF EXISTS `bliptv_data`;
+SET @saved_cs_client = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `bliptv_data` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `source_id` int(10) unsigned NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `uri` varchar(255) NOT NULL,
+  `show` varchar(255) NOT NULL,
+  `embed_uri` varchar(255) NOT NULL,
+  `embed` text NOT NULL,
+  `length` int(6) unsigned NOT NULL,
+  `thumbnail` varchar(255) NOT NULL,
+  `license` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `published` varchar(45) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `DUPLICATES` (`source_id`,`uri`),
+  FULLTEXT KEY `SEARCH` (`title`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
