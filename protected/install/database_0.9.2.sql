d15c2363f9035d96519a891340ee24a1767ccdf6
John Hobbs <john@velvetcache.org>
2010-03-08 10:28:42 -0600

    Added default icons for scrnshots and launchpad. Added to the install SQL as well.

--- protected/install/database_0.9.2.sql
+++ protected/install/database_0.9.2.sql
@@ -1174,3 +1174,43 @@ CREATE TABLE `scribd_data` (
   FULLTEXT KEY `SEARCH` (`content`,`title`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 SET character_set_client = @saved_cs_client;
+
+DROP TABLE IF EXISTS `launchpad_data`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `launchpad_data` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `source_id` int(10) unsigned NOT NULL,
+  `link` varchar(256) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `content` text NOT NULL,
+  `branch_url` text NOT NULL,
+  `branch` text NOT NULL,
+  `revision` varchar(256) NOT NULL,
+  `project` varchar(256) NOT NULL,
+  `message` text NOT NULL,
+  `published` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `DUPLICATES` USING BTREE (`source_id`,`link`),
+  FULLTEXT KEY `SEARCH` (`title`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+DROP TABLE IF EXISTS `scrnshots_data`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `scrnshots_data` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `source_id` int(10) unsigned NOT NULL,
+  `created_at` varchar(45) NOT NULL,
+  `title` text NOT NULL,
+  `description` text NOT NULL,
+  `link` varchar(256) NOT NULL,
+  `href` varchar(256) NOT NULL,
+  `thumbnail` varchar(256) NOT NULL,
+  `image` varchar(256) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `DUPLICATES` USING BTREE (`source_id`,`link`),
+  FULLTEXT KEY `SEARCH` (`description`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
--- protected/install/migration-0.9.2.sql
+++ protected/install/migration-0.9.2.sql
@@ -150,3 +150,41 @@ CREATE TABLE IF NOT EXISTS `scribd_data` (
   FULLTEXT KEY `SEARCH` (`content`,`title`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 SET character_set_client = @saved_cs_client;
+
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE IF NOT EXISTS `launchpad_data` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `source_id` int(10) unsigned NOT NULL,
+  `link` varchar(256) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `content` text NOT NULL,
+  `branch_url` text NOT NULL,
+  `branch` text NOT NULL,
+  `revision` varchar(256) NOT NULL,
+  `project` varchar(256) NOT NULL,
+  `message` text NOT NULL,
+  `published` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `DUPLICATES` USING BTREE (`source_id`,`link`),
+  FULLTEXT KEY `SEARCH` (`title`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE IF NOT EXISTS `scrnshots_data` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `source_id` int(10) unsigned NOT NULL,
+  `created_at` varchar(45) NOT NULL,
+  `title` text NOT NULL,
+  `description` text NOT NULL,
+  `link` varchar(256) NOT NULL,
+  `href` varchar(256) NOT NULL,
+  `thumbnail` varchar(256) NOT NULL,
+  `image` varchar(256) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `DUPLICATES` USING BTREE (`source_id`,`link`),
+  FULLTEXT KEY `SEARCH` (`description`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
